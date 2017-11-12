864b887117417d7025352be2f7eaf34b39bc71ae
omer03 <omer_03_@live.nl>
2017-11-12 17:19:41 +0100

    - Added ?butter command. Will be further worked on later.

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var count = 0;
 var cookieImg = "https://vignette.wikia.nocookie.net/creepypasta/images/8/81/Panel-cookie-choc-cookie.png/revision/latest?cb=20160111144002";
 var foreverAloneImg = "http://i3.kym-cdn.com/photos/images/newsfeed/000/150/505/f30fd24c56e1bcfc926883d6a51d5a00.gif";
 var questionMarkIcon = "https://n6-img-fp.akamaized.net/free-icon/white-question-mark-on-a-black-circular-background_318-35996.jpg?size=338&ext=jpg";
+var purposeImg = "https://images.lookhuman.com/render/standard/e9kzW1RKcbJOVn5b7gh9eL2KQzpctmZG/greetingcard45-off_white-z1-t-butter-robot.png";
 var butterEmote = "<:butter:376775465370779649>";
 var embed;
 var lastUser;
@@ -25,11 +26,13 @@ client.on('message', message => {
   	const command = args.shift().toLowerCase();
 
   	switch(command) {
+      case "butter":
+        message.channel.send(purposeImg);
+        break;
   		case "ping":
         message.channel.send('Pinging server...').then((msg) => {
         msg.edit(`Pong! Your latency is ${msg.createdTimestamp - message.createdTimestamp}ms.`);
         });
-
   			break;
   		case "hi":
         if (args[0] == "sexy") {
@@ -40,6 +43,7 @@ client.on('message', message => {
           message.channel.send(foreverAloneImg);
         } else if (args[0] == message.mentions.members.first() && args.length > 0 && args[0] != client.user) {
           message.channel.send(`${message.author} said hi to ${message.mentions.members.first()}:wave:`);
+          message.delete();
         } else if (args[0] == client.user) {
           message.channel.send("I won't talk to you.")
         } else {
@@ -50,9 +54,9 @@ client.on('message', message => {
         embed = new Discord.RichEmbed()
   			.addField(`${butterEmote}ButterBot for all your butter needs ${butterEmote}`,
           `Serving butter to **${client.users.size}** users in **${client.channels.size}** ` +
-          `channels on **${client.guilds.size}** servers.\n\n*I need more butter..*`)
+          `channels on **${client.guilds.size}** servers.\n\n*What is my purpose?*`)
           .setFooter("ButterBot v1.0 by omer03")
-        .setThumbnail(message.author.avatarURL)
+        .setThumbnail(purposeImg)
         .setColor("F3EF7D");
         message.channel.send(embed);
   			break;
@@ -100,7 +104,7 @@ client.on('message', message => {
   			embed = new Discord.RichEmbed()
   			.setAuthor("Commands List", questionMarkIcon)
         .setDescription("**`ping`**: Check your latency\n" +
-          "**`hi <mention>`**: Greet someone\n" +
+          "**`hi [mention]`**: Greet someone\n" +
           "**`about`**: About ButterBot\n" +
           "**`count`**: Add 1 to the counter\n" +
           "**`type`**: A typing minigame\n" +

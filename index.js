f68440c6320d1ae0e652a3ad4720fe8a618aca99
omer03 <omer_03_@live.nl>
2017-11-12 04:53:20 +0100

    - Tweaked some commands and changed ?help - Added ?type command, a typing minigame. Will be further worked on.

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var lastUser;
 
 client.on('ready', () => {
   console.log('I am ready!');
-  client.user.setGame("?help");
+  client.user.setGame("Type ?help");
 });
 
 client.on('message', message => {
@@ -26,22 +26,22 @@ client.on('message', message => {
 
   	switch(command) {
   		case "ping":
-        message.channel.send('Pong...').then((msg) => {
+        message.channel.send('Pinging server...').then((msg) => {
         msg.edit(`Pong! Your latency is ${msg.createdTimestamp - message.createdTimestamp}ms.`);
         });
 
   			break;
   		case "hi":
-  			if (args[0] == null) {
-          message.channel.send(`Please mention someone to be greeted.`);
+        if (args[0] == "sexy") {
+          message.channel.send("hey bby <3")
+        } else if (message.mentions.users.size == 0) {
+            message.reply(` please mention someone to be greeted.`);
         } else if (args[0] == message.author) {
           message.channel.send(foreverAloneImg);
         } else if (args[0] == message.mentions.members.first() && args.length > 0 && args[0] != client.user) {
           message.channel.send(`${message.author} said hi to ${message.mentions.members.first()}:wave:`);
         } else if (args[0] == client.user) {
           message.channel.send("I won't talk to you.")
-  			} else if (args[0] == "sexy") {
-          message.channel.send("hey bby <3")
         } else {
           return;
         }
@@ -51,6 +51,7 @@ client.on('message', message => {
   			.addField(`${butterEmote}ButterBot for all your butter needs ${butterEmote}`,
           `Serving butter to **${client.users.size}** users in **${client.channels.size}** ` +
           `channels on **${client.guilds.size}** servers.\n\n*I need more butter..*`)
+          .setFooter("ButterBot v1.0 by omer03")
         .setThumbnail(message.author.avatarURL)
         .setColor("F3EF7D");
         message.channel.send(embed);
@@ -67,14 +68,43 @@ client.on('message', message => {
         }
         lastUser = message.author;
   			break;
+      case "type":
+        let randomWord = Math.random().toString(36).replace(/[^a-z]+/g, '');
+        let timer = 3;
+        var interval;
+        message.channel.send("Generating a new word..")
+          .then((msg)=> {
+            interval = setInterval(function () {
+              msg.edit(`Starting in **${timer--}**..`)
+            }, 1000)
+          });
+        setTimeout(function() {
+          clearInterval(interval);
+          message.channel.send(randomWord)
+          .then(() => {
+            message.channel.awaitMessages(response => response.content === randomWord && response.author == message.author, {
+              max: 1,
+              time: 5000,
+              errors: ['time'],
+            })
+            .then((msg) => {
+              message.channel.send(`You got it in time!`);
+            })
+            .catch(() => {
+              message.channel.send('You\'re too slow.');
+            });
+          });
+        }, 4000);
+        break;
   		case "help":
   			embed = new Discord.RichEmbed()
   			.setAuthor("Commands List", questionMarkIcon)
-        .setDescription(`Prefix is **${prefix}**`)
-        .addField("Command", "`hi`\n`ping`\n`about`\n`count`\n`help`", true)
-        .addField("Description", "`Greet someone`\n`Check your latency`\n`About ButterBot`\n" +
-          "`Add 1 to the counter`\n`Commands list`", true)
-  			.setFooter("ButterBot v1.0 by omer03")
+        .setDescription("**`ping`**: Check your latency\n" +
+          "**`hi <mention>`**: Greet someone\n" +
+          "**`about`**: About ButterBot\n" +
+          "**`count`**: Add 1 to the counter\n" +
+          "**`type`**: A typing minigame\n" +
+          "**`help`**: Commands list")
         .setColor("F3EF7D");
   			message.channel.send(embed);
   			break;
